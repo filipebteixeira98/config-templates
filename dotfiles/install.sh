a01217ea1a751e13e394e23a64b973ba0c12dc02
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-01-13 19:05:48 -0300

    fix(apt): replace invalid Docker Ubuntu repo with Debian repo for bookworm

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -259,8 +259,8 @@ install_dev_tools() {
 
   sudo tee /etc/apt/sources.list.d/docker.sources <<-EOF
 	Types: deb
-	URIs: https://download.docker.com/linux/ubuntu
-	Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+	URIs: https://download.docker.com/linux/debian
+	Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
 	Components: stable
 	Signed-By: /etc/apt/keyrings/docker.asc
 	EOF
